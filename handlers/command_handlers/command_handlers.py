6798989cd59ffb009716b1a1252a6b5b1deb3d4a
JuryReimiS <indlinyury@gmail.com>
2021-04-20 16:07:41 +0300

    Добавлены рабочии хэндлеры для создания заготовки теста Добавлена инициализация класса Test

--- handlers/command_handlers/command_handlers.py
+++ handlers/command_handlers/command_handlers.py
@@ -1,4 +1,5 @@
 # Обработчики
+from aiogram.dispatcher import FSMContext
 
 from main import bot, dp
 from handlers.text_handlers.text_handlers import welcome_message
@@ -6,6 +7,8 @@ from keyboards.default import menu
 from aiogram.types import Message
 from config import admin_id
 from aiogram.dispatcher.filters import Command
+from states.creat_test.creat_test import CreateNameTest, CreatQuestions
+from utils.creat_test.creat_test import Test
 
 
 async def send_to_admin():
@@ -22,14 +25,30 @@ async def call_menu(message: Message):
     await message.answer("Чего надо?", reply_markup=menu)
 
 
-@dp.message_handler(Command("creat_test"))
-async def creat_test(message: Message):
+@dp.message_handler(Command("create_test"))
+async def create_test_name(message: Message):
     await message.reply(text="Напиши название теста")
+    await CreateNameTest.first()
 
 
+@dp.message_handler(state=CreateNameTest.NameIsCreating)
+async def create_test_len(message: Message, state: FSMContext):
+    name = str(message.text)
+    await state.update_data(name=name)
+    data = await state.get_data()
+    print(data)
+    await message.reply(text="Сколько будет вопросов?")
+    await CreateNameTest.next()
 
 
-
+@dp.message_handler(state=CreateNameTest.Test_len)
+async def ok(message: Message, state: FSMContext):
+    questions_quantity = int(message.text)
+    await state.update_data(questions_quantity=questions_quantity)
+    data = await state.get_data()
+    test = Test(test_name=data["name"], questions_quantity=data["questions_quantity"])
+    print(data)
+    await state.finish()
 
 
 
--- states/creat_test/creat_test.py
+++ states/creat_test/creat_test.py
@@ -1,9 +1,14 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
-class CreatTest(StatesGroup):
-    name_state = State()
+class CreateNameTest(StatesGroup):
+    NameIsCreating = State()
+    Test_len = State()
 
 
-
-
+class CreatQuestions(StatesGroup):
+    def __init__(self, questions_quantity):
+        classes = globals()
+        for i in range(questions_quantity):
+            name = f"State_{i+1}"
+            classes[name] = State()
